2b5c3c158ae531707c1330294df2e07915d234df
Steven Haddox <steven@haddox.us>
2014-11-19 10:43:20 -0500

    Added method documentation

--- lib/cert_munger/formatter.rb
+++ lib/cert_munger/formatter.rb
@@ -1,4 +1,6 @@
 require 'logging'
+
+# Custom exception for strings that can't be parsed as X509 Certificates
 class UnparsableCertError < TypeError; end
 
 #
@@ -6,24 +8,31 @@ class UnparsableCertError < TypeError; end
 # formatted certificate.
 #
 module CertMunger
+  # Enables `include CertMunger` to also extend class methods
   def self.included(base)
     base.extend(ClassMethods)
   end
 
+  # Instance variable accessor for CertMunger#to_cert
   def to_cert(raw_cert)
     self.class.send(:to_cert, raw_cert)
   end
 
   #
-  # Extended class methods via `include CertMunger`
+  # Class methods provided by CertMunger module
   #
   module ClassMethods
+    # logger method to return Rails logger if defined, else logging logger
     def logger
       return @logger if @logger
       logger = Logging.logger[self] unless @logger
       @logger ||= Kernel.const_defined?('Rails') ? Rails.logger : logger
     end
 
+    # Attempts to munge a string into a valid X509 certificate
+    #
+    # @param raw_cert [String] The string of text you wish to parse into a cert
+    # @return [OpenSSL::X509::Certificate]
     def to_cert(raw_cert)
       logger.debug "CertMunger raw_cert:\n#{raw_cert}"
       new_cert = build_cert(raw_cert)
@@ -33,6 +42,10 @@ module CertMunger
       OpenSSL::X509::Certificate.new new_cert
     end
 
+    # Creates a temporary cert and orchestrates certificate body reformatting
+    #
+    # @param raw_cert [String] The string of text you wish to parse into a cert
+    # @return [String] reformatted and (hopefully) parseable certificate string
     def build_cert(raw_cert)
       tmp_cert = ['-----BEGIN CERTIFICATE-----']
       if raw_cert.lines.count == 1
@@ -45,6 +58,10 @@ module CertMunger
       tmp_cert.join("\n").rstrip
     end
 
+    # Attempts to reformat one-line certificate bodies
+    #
+    # @param raw_cert [String] The string of text you wish to parse into a cert
+    # @return [String] reformatted certificate body
     def one_line_contents(raw_cert)
       cert_contents = raw_cert.split('\n')
       cert_contents.pop
@@ -52,6 +69,10 @@ module CertMunger
       cert_contents.map! { |el| el.match('\W+[t|n|r](.*)')[1] }
     end
 
+    # Attempts to reformat multi-line certificate bodies
+    #
+    # @param raw_cert [String] The string of text you wish to parse into a cert
+    # @return [String] reformatted certificate body
     def multi_line_contents(raw_cert)
       cert_contents = raw_cert.split(/[-](.*)[-]/)[2]
       cert_contents.lines.map do |line|
--- lib/cert_munger/string.rb
+++ lib/cert_munger/string.rb
@@ -1,8 +1,11 @@
-class String # rubocop:disable Documentation
+#
+# Extend the core String class to include `.to_cert` && `.to_cert!`
+#
+class String
   include CertMunger
 
   # Returns an X509 certificate after parsing the value of this object.
-  # Returns false if an X509 certificate cannot be created.
+  # Returns false if an X509 certificate cannot be created
   def to_cert
     begin
       new_cert = self.class.send(:to_cert, self)
@@ -14,7 +17,7 @@ class String # rubocop:disable Documentation
   end
 
   # Similar to {#to_cert}, but raises an error unless the string can be
-  # explicitly parsed to an X509 certifcate.
+  # explicitly parsed to an X509 certifcate
   def to_cert!
     begin
       new_cert = self.class.send(:to_cert, self)
