d2d232492bc3403449fdda802ffb11fb353bb862
Steven Haddox <steven@haddox.us>
2015-01-14 13:25:34 -0500

    Add HTML formatter to simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
 require 'coveralls'
-SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter,
+                        Coveralls::SimpleCov::Formatter]
 SimpleCov.start do
   add_filter '/spec/'
 end
