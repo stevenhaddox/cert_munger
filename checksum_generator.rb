ee8cc2d07eea73d4bd7fa65cf75ecdc2d5228485
Steven Haddox <steven@haddox.us>
2017-04-05 10:51:54 -0400

    Add checksum v0.2.1

---
+++ checksum_generator.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require 'digest/sha2'
+built_gem_path = "pkg/cert_munger-#{ENV['VERSION']}.gem"
+checksum = Digest::SHA512.new.hexdigest(File.read(built_gem_path))
+checksum_path = "checksum/cert_munger-#{ENV['VERSION']}.gem.sha512"
+File.open(checksum_path, 'w' ) {|f| f.write(checksum) }
