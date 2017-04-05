8ab705e70ed811b6b6afd08229897930c6ff160b
Steven Haddox <steven@haddox.us>
2017-04-05 10:56:11 -0400

    Fix checksum_generator for Rubocop

--- checksum_generator.rb
+++ checksum_generator.rb
@@ -4,4 +4,4 @@ require 'digest/sha2'
 built_gem_path = "pkg/cert_munger-#{ENV['VERSION']}.gem"
 checksum = Digest::SHA512.new.hexdigest(File.read(built_gem_path))
 checksum_path = "checksum/cert_munger-#{ENV['VERSION']}.gem.sha512"
-File.open(checksum_path, 'w' ) {|f| f.write(checksum) }
+File.open(checksum_path, 'w') { |f| f.write(checksum) }
